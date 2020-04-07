e4927d3229a7b70e2d728356607d8eb1e08bf796
Noah <drakene@g.cofc.edu>
2020-04-07 10:06:03 -0400

    Added code to data_preliminary_analysis.py that we can use to fetch the data from our sources.

--- data_preliminary_analysis.py
+++ data_preliminary_analysis.py
@@ -1,18 +1,39 @@
 import pandas as pd
+import os
+import sys
+
 import sklearn.pipeline
 import sklearn.model_selection
 
 
 def main():
+
+
     pd.set_option('display.max_rows', 500)
     pd.set_option('display.max_columns', None)
 
-    dataset_open_line = pd.read_csv('D:\\School Work\\CSCI 334\\Project\\Covid-19 Data\\COVID19_line_list_data.csv')
-    dataset_line_list = pd.read_csv('D:\\School Work\\CSCI 334\\Project\\Covid-19 Data\\COVID19_open_line_list.csv')
+    # Import New York Times data on states and counties
+    os.system('curl https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv'
+              ' > ' + '"' + os.path.join('Covid-19 Data', 'us-counties.csv') + '"')
+    os.system('curl https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv'
+              ' > ' + '"' + os.path.join('Covid-19 Data', 'us-states.csv') + '"')
 
-    print(dataset_line_list.columns)
-    print(dataset_open_line.columns)
+    # Import Johns Hopkins time series data
+    os.system('curl https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/'
+              'csse_covid_19_time_series/time_series_covid19_confirmed_US.csv'
+              ' > ' + '"' + os.path.join('Covid-19 Data', 'time_series_covid19_confirmed_US.csv') + '"')
+    os.system('curl https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/'
+              'csse_covid_19_time_series/time_series_covid19_deaths_US.csv'
+              ' > ' + '"' + os.path.join('Covid-19 Data', 'time_series_covid19_deaths_US.csv') + '"')
 
+    county_level_path = os.path.join('Covid-19 Data', 'us-counties.csv')
+    state_level_path = os.path.join('Covid-19 Data', 'us-states.csv')
+
+    county_level_dataset = pd.read_csv(county_level_path)
+    state_level_dataset = pd.read_csv(state_level_path)
+
+    print(county_level_dataset)
+    print(state_level_dataset)
 
 
 main()
