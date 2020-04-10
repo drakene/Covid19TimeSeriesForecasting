4b838ef8e93f7c5487c1ff2f3c33abffb117e683
Noah <drakene@g.cofc.edu>
2020-04-10 15:32:26 -0400

    Added a feature to request data from GitHub if the files have not been modified in 24 hours upon execution of data_preliminary_analysis.py

--- data_preliminary_analysis.py
+++ data_preliminary_analysis.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import time
 import sys
 
 import sklearn.pipeline
@@ -8,32 +9,43 @@ import sklearn.model_selection
 
 def main():
 
-
     pd.set_option('display.max_rows', 500)
     pd.set_option('display.max_columns', None)
 
-    # Import New York Times data on states and counties
-    os.system('curl https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv'
-              ' > ' + '"' + os.path.join('Covid-19 Data', 'us-counties.csv') + '"')
-    os.system('curl https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv'
-              ' > ' + '"' + os.path.join('Covid-19 Data', 'us-states.csv') + '"')
-
-    # Import Johns Hopkins time series data
-    os.system('curl https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/'
-              'csse_covid_19_time_series/time_series_covid19_confirmed_US.csv'
-              ' > ' + '"' + os.path.join('Covid-19 Data', 'time_series_covid19_confirmed_US.csv') + '"')
-    os.system('curl https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/'
-              'csse_covid_19_time_series/time_series_covid19_deaths_US.csv'
-              ' > ' + '"' + os.path.join('Covid-19 Data', 'time_series_covid19_deaths_US.csv') + '"')
-
+    # Data sets
     county_level_path = os.path.join('Covid-19 Data', 'us-counties.csv')
     state_level_path = os.path.join('Covid-19 Data', 'us-states.csv')
+    timeseries_cases = os.path.join('Covid-19 Data', 'time_series_covid19_confirmed_US.csv')
+    timeseries_deaths = os.path.join('Covid-19 Data', 'time_series_covid19_deaths_US.csv')
+
+    if os.path.getmtime(county_level_path) - time.time() > 86400:
+
+        print('Downloading raw data...')
+
+        # Import New York Times data on states and counties
+        os.system('curl https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv'
+                  ' > ' + '"' + county_level_path + '"')
+        os.system('curl https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv'
+                  ' > ' + '"' + state_level_path + '"')
+
+        # Import Johns Hopkins time series data
+        os.system('curl https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/'
+                  'csse_covid_19_time_series/time_series_covid19_confirmed_US.csv'
+                  ' > ' + '"' + timeseries_cases + '"')
+        os.system('curl https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/'
+                  'csse_covid_19_time_series/time_series_covid19_deaths_US.csv'
+                  ' > ' + '"' + timeseries_deaths + '"')
+
+        print('Finished downloading raw data.')
 
     county_level_dataset = pd.read_csv(county_level_path)
     state_level_dataset = pd.read_csv(state_level_path)
-
     print(county_level_dataset)
     print(state_level_dataset)
+
+    print('______________________________STATE OF WYOMING______________________________')
+    print(county_level_dataset.loc[(county_level_dataset['state'] == 'Wyoming') &
+                                   (county_level_dataset['county'] == 'Sheridan')])
 
 
 main()
